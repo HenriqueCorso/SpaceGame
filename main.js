d746206d9991995e796a3391250ae1c6836c08da
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-21 14:21:16 -0300

    Update main.js
    Added PlayerShip and Asteroid to the canvas

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const main = () => {
+import { PlayerShip } from './ship.js';
+import { Asteroid } from './asteroids.js';
 
+
+const main = () => {
   class Game {
     constructor() {
       this.canvas = document.getElementById('canvas');
@@ -7,26 +10,29 @@ const main = () => {
       this.canvas.width = window.innerWidth;
       this.canvas.height = window.innerHeight;
 
-      this.context.fillStyle = 'black'
-      this.context.fillRect(0, 0, canvas.width, canvas.height);
+      this.context.fillStyle = 'black';
+      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.player = new PlayerShip({ x: 100, y: 100 }); // add PlayerShip to canvas
+      this.asteroid = new Asteroid({ x: 100, y: 100 }, { x: 2, y: 2 }, 20); // create an instance of Asteroid
 
 
+      setInterval(() => this.updateGame(), 1000 / 60);
+    }
 
-      setInterval(1000 / 60, () => updateGame());
+    updateGame() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-      const updateGame = () => {
-        this.context.draw(backgroundImage, 0, 0);   // draw the background image on canvas to "clear" the screen
-        ship.update();                     // update the position of the ship (calls the method of the PlayerShip class)
-        ship.draw();                       // draw the ship
-      }
+      this.player.update(this.context); // update method of the ship
+      this.player.draw(this.context); // draw method of the ship
 
+      this.asteroid.update(this.context); //update method of the asteroid
+      this.asteroid.draw(this.context); // draw method of the asteroid
     }
   }
 
   const game = new Game();
-  game.init();
-  game.start();
 }
-
 
 main();
