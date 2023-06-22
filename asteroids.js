6a3da852b8ea87c462c1de9dc2140de842ba4816
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-22 18:14:34 -0300

    Update asteroids.js
    asteroids wraping around the screen

--- asteroids.js
+++ asteroids.js
@@ -7,9 +7,11 @@ export class Asteroid {
 
   draw(context) {
     context.resetTransform();
+
     context.beginPath();
     context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
     context.closePath();
+
     context.strokeStyle = 'white';
     context.stroke();
   }
@@ -17,5 +19,18 @@ export class Asteroid {
   update() {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
+    // Wrap around the screen
+    if (this.position.x < -this.radius) {
+      this.position.x = canvas.width + this.radius;
+    } else if (this.position.x > canvas.width + this.radius) {
+      this.position.x = -this.radius;
+    }
+
+    if (this.position.y < -this.radius) {
+      this.position.y = canvas.height + this.radius;
+    } else if (this.position.y > canvas.height + this.radius) {
+      this.position.y = -this.radius;
+    }
   }
 }
