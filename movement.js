d4c4bdcf0911e622a8b7d7d963d463f946d47243
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-21 22:12:14 -0300

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,13 +1,18 @@
+import { Projectile } from './projectile.js';
+
 export class Movement {
   constructor(playerShip) {
     this.playerShip = playerShip;
     this.keyState = {};
+    this.projectiles = []; // Initialize the projectiles array
+    this.PROJECTILE_SPEED = 5;
     this.registerEventListeners();
   }
 
   registerEventListeners() {
     document.addEventListener('keydown', (event) => this.handleKeyDown(event));
     document.addEventListener('keyup', (event) => this.handleKeyUp(event));
+    document.addEventListener('keydown', (event) => this.handleSpaceBar(event));
   }
 
   handleKeyDown(event) {
@@ -20,17 +25,41 @@ export class Movement {
     this.updateMovement();
   }
 
+
+  handleSpaceBar(event) {
+    if (event.code === 'Space') {
+      this.projectiles.push(
+        new Projectile({
+          position: {
+            x:
+              this.playerShip.position.x +
+              Math.cos(this.playerShip.rotation) * 30,
+            y:
+              this.playerShip.position.y +
+              Math.sin(this.playerShip.rotation) * 30,
+          },
+          velocity: {
+            x: Math.cos(this.playerShip.rotation) * this.PROJECTILE_SPEED,
+            y: Math.sin(this.playerShip.rotation) * this.PROJECTILE_SPEED,
+          },
+        })
+      );
+    }
+  }
+
   updateMovement() {
     const { keyState, playerShip } = this;
 
     if (keyState['KeyW']) {
-      const speed = 2;
+      const speed = 3;
       const angle = playerShip.rotation;
       const velocityX = speed * Math.cos(angle);
       const velocityY = speed * Math.sin(angle);
       playerShip.velocity = { x: velocityX, y: velocityY };
     } else {
-      playerShip.velocity = { x: 0, y: 0 };
+      playerShip.velocity.x *= 0.9
+      playerShip.velocity.y *= 0.9
+
     }
 
     if (keyState['KeyA']) {
