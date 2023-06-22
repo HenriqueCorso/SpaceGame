5b86cc84e750f380ecac9b3456e982ec0416e1ca
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-22 13:47:46 -0300

    Update movement.js
    added game.shootProjectile to the space bar

--- movement.js
+++ movement.js
@@ -1,18 +1,14 @@
-import { Projectile } from './projectile.js';
-
 export class Movement {
-  constructor(playerShip) {
+  constructor(playerShip, game) {
     this.playerShip = playerShip;
+    this.game = game;
     this.keyState = {};
-    this.projectiles = []; // Initialize the projectiles array
-    this.PROJECTILE_SPEED = 5;
     this.registerEventListeners();
   }
 
   registerEventListeners() {
     document.addEventListener('keydown', (event) => this.handleKeyDown(event));
     document.addEventListener('keyup', (event) => this.handleKeyUp(event));
-    document.addEventListener('keydown', (event) => this.handleSpaceBar(event));
   }
 
   handleKeyDown(event) {
@@ -25,30 +21,8 @@ export class Movement {
     this.updateMovement();
   }
 
-
-  handleSpaceBar(event) {
-    if (event.code === 'Space') {
-      this.projectiles.push(
-        new Projectile({
-          position: {
-            x:
-              this.playerShip.position.x +
-              Math.cos(this.playerShip.rotation) * 30,
-            y:
-              this.playerShip.position.y +
-              Math.sin(this.playerShip.rotation) * 30,
-          },
-          velocity: {
-            x: Math.cos(this.playerShip.rotation) * this.PROJECTILE_SPEED,
-            y: Math.sin(this.playerShip.rotation) * this.PROJECTILE_SPEED,
-          },
-        })
-      );
-    }
-  }
-
   updateMovement() {
-    const { keyState, playerShip } = this;
+    const { keyState, playerShip, game } = this;
 
     if (keyState['KeyW']) {
       const speed = 3;
@@ -57,17 +31,20 @@ export class Movement {
       const velocityY = speed * Math.sin(angle);
       playerShip.velocity = { x: velocityX, y: velocityY };
     } else {
-      playerShip.velocity.x *= 0.9
-      playerShip.velocity.y *= 0.9
-
+      playerShip.velocity.x *= 0.9;
+      playerShip.velocity.y *= 0.9;
     }
 
     if (keyState['KeyA']) {
-      playerShip.rotation -= 0.3;
+      playerShip.rotation -= 0.2;
     }
 
     if (keyState['KeyD']) {
-      playerShip.rotation += 0.3;
+      playerShip.rotation += 0.2;
+    }
+
+    if (keyState['Space']) {
+      game.shootProjectile();
     }
   }
 }
