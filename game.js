13ceaace71ba4b1774a4657bbd284ee003608c5c
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-22 13:46:42 -0300

    Update game.js
    added shootProjectile
    added loop for projectiles
    added remove projectiles

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ import { Asteroid } from './asteroids.js';
 import { Movement } from './movement.js';
 import { Projectile } from './projectile.js';
 
-
-
 export class Game {
   constructor() {
     this.canvas = document.getElementById('canvas');
@@ -20,11 +18,11 @@ export class Game {
       velocity: { x: 0, y: 0 }
     });
 
-    this.movement = new Movement(this.player);
+    this.movement = new Movement(this.player, this);
 
     this.asteroid = new Asteroid({ x: 100, y: 100 }, { x: 2, y: 2 }, 20);
 
-    this.projectiles = []; // Initialize the projectiles array
+    this.projectiles = []; // Array to store projectiles
   }
 
   startGame() {
@@ -37,31 +35,49 @@ export class Game {
     clearInterval(this.gameLoopInterval);
   }
 
+  shootProjectile() {
+    const speed = 5;
+    const angle = this.player.rotation;
+    const velocityX = speed * Math.cos(angle);
+    const velocityY = speed * Math.sin(angle);
+
+    const projectile = new Projectile(
+      {
+        x: this.player.position.x + Math.cos(this.player.rotation) * 30,
+        y: this.player.position.y + Math.sin(this.player.rotation) * 30
+      },
+      { x: velocityX, y: velocityY }
+    );
+
+    this.projectiles.push(projectile);
+  }
+
   updateGame() {
     this.context.resetTransform();
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.player.update(this.context);
-    this.player.draw(this.context);
+    this.player.update(this.context); // update method of the ship
+    this.player.draw(this.context); // draw method of the ship
 
-    this.asteroid.update(this.context);
-    this.asteroid.draw(this.context);
+    this.asteroid.update(this.context); // update method of the asteroid
+    this.asteroid.draw(this.context); // draw method of the asteroid
 
     for (let i = this.projectiles.length - 1; i >= 0; i--) {
-      const projectile = this.projectiles[i];
-      projectile.update();
-      projectile.draw();
+      const projectile = this.projectiles[i]
+      projectile.update(); // update method of the projectile
+      projectile.draw(this.context); // draw method of the projectile
 
-      // Remove projectiles that are off-screen or expired
-      if (
-        projectile.position.x > this.canvas.width ||
-        projectile.position.y > this.canvas.height ||
-        projectile.position.x < 0 ||
-        projectile.position.y < 0
+      // remove projectiles  
+      if (projectile.position.x + projectile.radius < 0 ||
+        projectile.position.x - projectile.radius > this.canvas.width ||
+        projectile.position.y - projectile.radius > this.canvas.height ||
+        projectile.position.y + projectile.radius < 0
       ) {
-        this.projectiles.splice(j, 1);
-      }
+        this.projectiles.splice(i, 1);
+
+
+      };
     }
   }
 }
