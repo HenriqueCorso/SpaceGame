52aeb8e28942196b91ac729c1efe7628bf0c5cae
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-27 15:36:49 -0300

    Update ship.js

--- ship.js
+++ ship.js
@@ -3,6 +3,8 @@ export class PlayerShip {
     this.position = position; // {x, y}
     this.velocity = velocity;
     this.rotation = 0;
+    this.isInvulnerable = false
+    this.isVisible = true
   }
 
   draw(context) {
@@ -16,8 +18,43 @@ export class PlayerShip {
     context.lineTo(-10, 10);
     context.closePath();
 
-    context.lineWidth = 2
+    context.lineWidth = 2;
     context.strokeStyle = 'white';
+    context.stroke();
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+  }
+}
+
+export class EnemyShip {
+  constructor({ position, velocity }) {
+    this.position = position; // {x, y}
+    this.velocity = velocity;
+    this.rotation = 0;
+    this.radius = 15; // Adjust the size of the enemy ship
+  }
+
+  draw(context) {
+    context.resetTransform();
+
+    // Set the color and style for the enemy ship
+    context.fillStyle = 'red';
+    context.strokeStyle = 'white';
+    context.lineWidth = 2;
+
+    // Draw the alien ship shape
+    context.beginPath();
+    context.moveTo(this.position.x, this.position.y - this.radius);
+    context.lineTo(this.position.x - this.radius, this.position.y + this.radius);
+    context.lineTo(this.position.x + this.radius, this.position.y + this.radius);
+    context.closePath();
+
+    // Fill and stroke the shape to complete the drawing
+    context.fill();
     context.stroke();
   }
 
@@ -26,4 +63,3 @@ export class PlayerShip {
     this.position.y += this.velocity.y;
   }
 }
-
