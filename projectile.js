b3c7689b744c7103363b9c8e9e7fd1c8a935bb13
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-06-27 13:10:28 -0300

    Update projectile.js
    added EnemyProjectile

--- projectile.js
+++ projectile.js
@@ -19,3 +19,25 @@ export class Projectile {
     this.position.y += this.velocity.y;
   }
 }
+
+export class EnemyProjectile {
+  constructor(position, velocity) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 2;
+  }
+
+  draw(context) {
+    context.resetTransform();
+    context.beginPath();
+    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    context.closePath();
+    context.strokeStyle = 'red';
+    context.stroke();
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
